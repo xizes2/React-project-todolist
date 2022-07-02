346d1a3813ba9a03fa6db87d4b8340a106fd508f
xizes2 <xizes2@hotmail.com>
2022-07-02 11:36:38 +0200

    final commit - set styles'n actions for task list

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import "./App.css";
+//import loader from "../public/loading.svg";
 
 import { useState, useEffect } from "react";
 import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from "react-icons/bs";
-
-import FirstComponent from "./components/FirstComponent";
 
 const API = "http://localhost:5000";
 
 function App() {
   const [title, setTitle] = useState("");
   const [time, setTime] = useState("");
-  const [todos, setTodos] = useState([]);
+  const [toDos, setToDos] = useState([]);
   const [loading, setLoading] = useState(false);
 
   // Load todolist on page load
+  useEffect(() => {
+    const loadData = async () => {
+      setLoading(true);
+
+      const res = await fetch(API + "/todos")
+        .then((res) => res.json())
+        .then((data) => data)
+        .catch((err) => console.log(err));
+
+      setLoading(false);
+
+      setToDos(res);
+    };
+
+    loadData();
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -34,9 +49,40 @@ function App() {
 
     console.log(toDoTask);
 
+    // Send new toDoTask to previous estate of toDos, so we can keep the 'to do list' always updated
+    setToDos((prevState) => [...prevState, toDoTask]);
+
     setTitle("");
     setTime("");
   };
+
+  const handleDelete = async (id) => {
+    await fetch(API + "/todos/" + id, {
+      method: "DELETE",
+    });
+
+    setToDos((prevState) => prevState.filter((toDo) => toDo.id !== id));
+  };
+
+  const handleTaskDone = async (toDo) => {
+    toDo.done = !toDo.done;
+
+    const data = await fetch(API + "/todos/" + toDo.id, {
+      method: "PUT",
+      body: JSON.stringify(toDo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    setToDos((prevState) =>
+      prevState.map((toDo) => (toDo.id === data.id ? (toDo = data) : toDo))
+    );
+  };
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div className="App">
@@ -65,7 +111,7 @@ function App() {
             <input
               type="text"
               name="time"
-              placeholder="Task estimated time (in hours)"
+              placeholder="Task estimated time (hours)"
               onChange={(e) => setTime(e.target.value)}
               value={time || ""}
               required
@@ -78,7 +124,21 @@ function App() {
 
       <div className="list-todo">
         <h2>Lista de Tarefas:</h2>
-        {todos.length === 0 && <p>No tasks registered!</p>}
+
+        {toDos.length === 0 && <p>No tasks registered!</p>}
+
+        {toDos.map((toDo) => (
+          <div className="toDo" key={toDo.id}>
+            <h3 className={toDo.done ? "toDo-done" : ""}> {toDo.title} </h3>
+            <p>Time estimated: {toDo.time}h </p>
+            <div className="actions">
+              <span onClick={() => handleTaskDone(toDo)}>
+                {toDo.done ? <BsBookmarkCheckFill /> : <BsBookmarkCheck />}
+              </span>
+              <BsTrash onClick={() => handleDelete(toDo.id)} />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
